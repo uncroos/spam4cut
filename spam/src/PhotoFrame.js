7e5ae78e6d5825bc4b4da92dacb0889640d70757
UNCROOS <any492266@gmail.com>
2024-09-05 01:24:36 +0900

    sd

--- spam/src/PhotoFrame.js
+++ spam/src/PhotoFrame.js
@@ -4,17 +4,15 @@ import "./PhotoFrame.css";
 const PhotoFrame = ({ photos }) => {
   return (
     <div className="photo-frame">
-      <div className="photo-container">
-        {photos.map((photo, index) => (
-          <img
-            key={index}
-            src={photo}
-            alt={`사진 ${index + 1}`}
-            style={{ width: "100%", height: "auto" }}
-          />
-        ))}
-        <div className="spam-area">SPAM</div>
-      </div>
+      {photos.map((photo, index) => (
+        <img
+          key={index}
+          src={photo}
+          alt={`사진 ${index + 1}`}
+          style={{ width: "100%", height: "100%", objectFit: "cover" }}
+        />
+      ))}
+      {photos.length > 0 && <div className="spam-area">SPAM</div>}
     </div>
   );
 };
